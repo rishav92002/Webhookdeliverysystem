acf5d33bb35b9495ac6001dba8ef4564836c71b3
rishav92002 <technork2002@gmail.com>
2026-08-09 17:16:16 +0530

    completed customer related endpoints

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import { customerSchema } from "../validators/schemaValidators.js";
-import { createCustomerService } from "../services/customer.service.js";
+import { createCustomerService, getCustomerService, updateCustomerService, deleteCustomerService, regenerateApiKeyService } from "../services/customer.service.js";
 
 export const createCustomerController = async (req:Request, res:Response) => {
     const customerDetail = customerSchema.safeParse(req.body);
@@ -11,9 +11,41 @@ export const createCustomerController = async (req:Request, res:Response) => {
     }
 
     const customer = await createCustomerService(customerDetail.data);
-    return res.status(202).json({
-        customerId: customer.customerId,
-        name : customer.name,
-        apiKey: customer.apiKey,
-    });
+    return res.status(200).json(customer);   
+}
+export const regenerateApiKeyController = async (req:Request, res:Response) => {
+    if(!req.customerId){
+        return res.status(401).json({message:'Unauthorized'});
+    }
+
+    const customer = await regenerateApiKeyService(req.customerId);
+    return res.status(200).json(customer);   
+}
+
+
+export const getCustomerController = async (req:Request, res:Response) => {
+    if(!req.customerId){
+        return res.status(401).json({message:'Unauthorized'});
+    }
+    const customer = await getCustomerService(req.customerId);
+    return res.status(200).json(customer);
+}
+export const updateCustomerController = async (req:Request, res:Response) => {
+    if(!req.customerId){
+        return res.status(401).json({message:'Unauthorized'});
+    }
+    const {name} = req.body;
+
+    if(!name){
+        return res.status(400).json({message:'Name is required'});
+    }
+    const customer = await updateCustomerService(req.customerId, name);
+    return res.status(200).json(customer);
+}
+export const deleteCustomerController = async (req:Request, res:Response) => {
+    if(!req.customerId){
+        return res.status(401).json({message:'Unauthorized'});
+    }
+    const customer = await deleteCustomerService(req.customerId);
+    return res.status(200).json(customer);
 }
